7ced4c8d73ce71dc8b017903096e951ad496bebd
Anderson Meira <anderson.meira@gmail.com>
2017-03-09 11:01:11 -0300

    working with props

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			deadline: 'December 25, 2017',
+			newDeadline: ''
+		}
+	}
+
+	changeDeadline() {
+		this.setState({deadline: this.state.newDeadline})
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App-title">
+					Countdown to {this.state.deadline}
+				</div>
+				<Clock deadline={this.state.deadline} />
+				<div>
+					<input onChange={event => this.setState({newDeadline: event.target.value})} placeholder="new date" />
+					<button onClick={() => this.changeDeadline()}>Submit</button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default App;
